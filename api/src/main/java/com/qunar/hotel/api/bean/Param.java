290cca49d97bc8bc2f8ac88819025a5a7971db2e
jianyu.lin <jianyu.lin@qunar.com>
2016-08-19 16:47:20 +0800

    1. 增加rabbitmq测试类

--- api/src/main/java/com/qunar/hotel/api/bean/Param.java
+++ api/src/main/java/com/qunar/hotel/api/bean/Param.java
@@ -8,7 +8,6 @@ import java.io.Serializable;
 public class Param implements Serializable {
     private String id;
     private String name;
-    private Type type;
 
     public String getId() {
         return id;
@@ -26,20 +25,11 @@ public class Param implements Serializable {
         this.name = name;
     }
 
-    public Type getType() {
-        return type;
-    }
-
-    public void setType(Type type) {
-        this.type = type;
-    }
-
     @Override
     public String toString() {
         return "Param{" +
                 "id='" + id + '\'' +
                 ", name='" + name + '\'' +
-                ", type=" + type +
                 '}';
     }
 }
